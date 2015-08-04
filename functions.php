1003e60f94861bc0a50fddd2d2dbc1f1748f3452
JigmeDatse <info@datsemultimedia.com>
2015-08-04 15:49:09 -0700

    Moved stuff to root of git project.
    Got stuff in the root of the git project.  This should work.

---
+++ functions.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Created on Aug 3, 2015
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+function theme_enqueue_styles() {
+
+	$parent_style = 'thematic';
+
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+			get_stylesheet_directory_uri() . '/style.css',
+			array( $parent_style )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+?>
