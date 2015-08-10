f299a16a0b2fa07f92f86f05afe8f7c5026e0d90
JigmeDatse <info@datsemultimedia.com>
2015-08-09 17:16:04 -0700

    This should be WooCommerce support enabled.  Let's try it out.

--- functions.php
+++ functions.php
@@ -18,4 +18,18 @@ function theme_enqueue_styles() {
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
+
 ?>
